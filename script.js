e649451f98bd4dcafbb7d2e20b340d6619fa3e8c
TsvetkovM <149527029+TsvetkovM@users.noreply.github.com>
2024-11-08 12:10:51 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,24 @@
+const result = 5+5+'5';
+
+console.log(result);
+console.log(typeof result);
+// 2============
+const email = 'yourname@gmail.com';
+
+console.log(email.includes('@'));
+console.log(email.length);
+// 3=============
+const word1 = 'My';
+const word2 = 'name';
+const word3 = 'is';
+let fullName;
+
+fullName = word1 +' '+ word2 + ' ' + word3+ ' ' + 'Victor';
+
+console.log(fullName);
+// 4==============
+const userName = 'Олександр';
+const payment = '300';
+alert(`Дякуємо, ${userName}о! До сплати ${payment} гривень`);
+
+
